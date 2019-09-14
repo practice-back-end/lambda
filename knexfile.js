89f196527c6dee18901aed8c03bc7eed066261ae
amlane <twelvexstring@gmail.com>
2019-09-14 00:15:02 -0500

    whoops forgot my seeds

--- knexfile.js
+++ knexfile.js
@@ -47,6 +47,9 @@ module.exports = {
     migrations: {
       directory: './data/migrations',
       tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   }
 
